a46ed7fe22ac1c77e12cf6f3f664264f8885e8ee
Tim Peters <t.p.peters@student.tudelft.nl>
2021-02-06 18:08:56 +0000

    Created project + checkstyle + main fragments

---
+++ frontend/app/src/main/java/com/group2/recipeze/MainActivity.java
@@ -0,0 +1,35 @@
+package com.group2.recipeze;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+/**
+ * MainActivity.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_feed,
+                R.id.navigation_explore,
+                R.id.navigation_addRecipe,
+                R.id.navigation_recipeBook,
+                R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
