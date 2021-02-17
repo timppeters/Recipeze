fef82c2968713d5903c91c6350f32db58154582a
Tim Peters <t.p.peters@student.tudelft.nl>
2021-02-17 16:12:37 +0000

    Added imageSlider + custom scroll stuff for recipe page

--- frontend/app/src/main/java/com/group2/recipeze/ui/recipe/RecipeFragment.java
+++ frontend/app/src/main/java/com/group2/recipeze/ui/recipe/RecipeFragment.java
@@ -1,19 +1,26 @@
 package com.group2.recipeze.ui.recipe;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.TypedValue;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 
@@ -32,6 +39,8 @@ import java.util.List;
 public class RecipeFragment extends Fragment {
 
     private RecipeViewModel recipeViewModel;
+    private boolean scrollable = false;
+    private int originalTopMargin;
 
     /**
      * Called when view is created.
@@ -68,6 +77,53 @@ public class RecipeFragment extends Fragment {
         slideModels.add(new SlideModel("https://live.staticflickr.com/7006/6621416427_8504865e6a_z.jpg",ScaleTypes.CENTER_CROP));
         slideModels.add(new SlideModel("https://c4.wallpaperflare.com/wallpaper/662/618/496/natur-2560x1600-sceneries-wallpaper-preview.jpg",ScaleTypes.CENTER_CROP));
         imageSlider.setImageList(slideModels, ScaleTypes.CENTER_CROP);
+
+        LinearLayout contentParent = root.findViewById(R.id.contentParent);
+        ScrollView content = root.findViewById(R.id.content);
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) contentParent.getLayoutParams();
+        originalTopMargin = layoutParams.topMargin;
+
+        float dip = 60f; // size of topbar
+        Resources r = getResources();
+        float marginMin = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dip,
+                r.getDisplayMetrics()
+        );
+
+        content.setOnTouchListener(new View.OnTouchListener() {
+
+            float dY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) contentParent.getLayoutParams();
+                int marginDelta = layoutParams.topMargin;
+                if (marginDelta > marginMin && marginDelta <= originalTopMargin || (marginDelta == marginMin && content.getScrollY() == 0)) { // block scrolling
+                    switch (event.getAction()) {
+
+                        case MotionEvent.ACTION_DOWN:
+
+                            dY = contentParent.getY() - event.getRawY();
+                            break;
+
+                        case MotionEvent.ACTION_MOVE:
+                            int newMargin = (int) (event.getRawY() + dY);
+                            if (newMargin >= marginMin && newMargin <= originalTopMargin) {
+                                layoutParams.topMargin = newMargin;
+                                contentParent.setLayoutParams(layoutParams);
+                                content.setScrollY(0);
+                            }
+
+                            break;
+                        default:
+                            return true;
+                    }
+                }
+                return false; // allow scrolling
+            }
+        });
+
 
         return root;
 
