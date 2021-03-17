760cbeb836523572d028e8a2db86a776d7822cfb
Tim Peters <t.p.peters@student.tudelft.nl>
2021-03-17 15:55:43 +0000

    Some implementation for getting recipe

--- frontend/app/src/main/java/com/group2/recipeze/ui/recipe/RecipeFragment.java
+++ frontend/app/src/main/java/com/group2/recipeze/ui/recipe/RecipeFragment.java
@@ -1,26 +1,28 @@
 package com.group2.recipeze.ui.recipe;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.widget.NestedScrollView;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.TypedValue;
-import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 
@@ -28,7 +30,8 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.group2.recipeze.R;
-import com.group2.recipeze.ui.recipeBook.RecipeBookViewModel;
+import com.group2.recipeze.data.RecipeRepository;
+import com.group2.recipeze.data.model.Recipe;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +44,8 @@ public class RecipeFragment extends Fragment {
     private RecipeViewModel recipeViewModel;
     private boolean scrollable = false;
     private int originalTopMargin;
+    private MutableLiveData<Recipe> recipe = new MutableLiveData<Recipe>();
+    private RecipeRepository recipeRepository;
 
     /**
      * Called when view is created.
@@ -50,6 +55,7 @@ public class RecipeFragment extends Fragment {
      * @param savedInstanceState
      * @return
      */
+    @SuppressLint("ClickableViewAccessibility")
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         recipeViewModel =
@@ -57,32 +63,22 @@ public class RecipeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_recipe, container, false);
 
 
+        // Get recipeId from whoever called fragment
         Integer recipeId = getArguments().getInt("recipeId");
+        recipeRepository = RecipeRepository.getInstance();
 
-        final TextView prepTimeValue = root.findViewById(R.id.prepTimeValue);
-        final TextView cookTimeValue = root.findViewById(R.id.cookTimeValue);
-
-        recipeViewModel.getPrepTime().observe(getViewLifecycleOwner(), new Observer<String>() {
+        recipe.observe(getViewLifecycleOwner(), new Observer<Recipe>() {
             @Override
-            public void onChanged(@Nullable String s) {  prepTimeValue.setText(s); }
+            public void onChanged(Recipe recipe) {
+                showRecipe(recipe, root);
+            }
         });
 
-        recipeViewModel.getCookTime().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {  cookTimeValue.setText(s); }
-        });
-
-        ImageSlider imageSlider = root.findViewById(R.id.slider);
-
-        List<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel("https://p.ecopetit.cat/wpic/lpic/26-263518_tumblr-photography-wallpaper-rocks-on-earth-background.jpg",ScaleTypes.CENTER_CROP));
-        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",ScaleTypes.CENTER_CROP));
-        slideModels.add(new SlideModel("https://live.staticflickr.com/7006/6621416427_8504865e6a_z.jpg",ScaleTypes.CENTER_CROP));
-        slideModels.add(new SlideModel("https://c4.wallpaperflare.com/wallpaper/662/618/496/natur-2560x1600-sceneries-wallpaper-preview.jpg",ScaleTypes.CENTER_CROP));
-        imageSlider.setImageList(slideModels, ScaleTypes.CENTER_CROP);
+        // get recipe from db
+        recipeRepository.getRecipe(recipeId, recipe);
 
         LinearLayout contentParent = root.findViewById(R.id.contentParent);
-        ScrollView content = root.findViewById(R.id.content);
+        NestedScrollView content = root.findViewById(R.id.content);
         ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) contentParent.getLayoutParams();
         originalTopMargin = layoutParams.topMargin;
 
@@ -102,9 +98,9 @@ public class RecipeFragment extends Fragment {
             public boolean onTouch(View v, MotionEvent event) {
                 ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) contentParent.getLayoutParams();
                 int marginDelta = layoutParams.topMargin;
-                if (marginDelta > marginMin && marginDelta <= originalTopMargin || (marginDelta == marginMin && content.getScrollY() == 0)) { // block scrolling
+                if (marginDelta > marginMin && marginDelta < originalTopMargin || (marginDelta == marginMin && content.getScrollY() == 0)) { // block scrolling
+                    System.out.println(event.getAction());
                     switch (event.getAction()) {
-
                         case MotionEvent.ACTION_DOWN:
 
                             dY = contentParent.getY() - event.getRawY();
@@ -115,7 +111,7 @@ public class RecipeFragment extends Fragment {
                             if (newMargin >= marginMin && newMargin <= originalTopMargin) {
                                 layoutParams.topMargin = newMargin;
                                 contentParent.setLayoutParams(layoutParams);
-                                content.setScrollY(0);
+                                //content.setScrollY(0);
                             }
 
                             break;
@@ -130,5 +126,38 @@ public class RecipeFragment extends Fragment {
 
         return root;
 
+    }
+
+    // show recipe in UI
+    public void showRecipe(Recipe recipe, View root) {
+        final TextView prepTimeValue = root.findViewById(R.id.prepTimeValue);
+        final TextView cookTimeValue = root.findViewById(R.id.cookTimeValue);
+        final HorizontalScrollView tags = root.findViewById(R.id.tags);
+        final TextView recipeName = root.findViewById(R.id.recipeName);
+        final TextView recipeDescription = root.findViewById(R.id.recipeDescription);
+        final RecyclerView ingredientsList = root.findViewById(R.id.ingredientsList);
+        final RecyclerView stepsList = root.findViewById(R.id.stepList);
+
+        prepTimeValue.setText(String.valueOf(recipe.getPrepTime()) + " mins");
+        cookTimeValue.setText(String.valueOf(recipe.getCookTime()) + " mins");
+        recipeName.setText(recipe.getTitle());
+        recipeDescription.setText(recipe.getDescription());
+        if (recipe.getTags() != null) {
+            for (String tagName : recipe.getTags()) {
+                TextView tagView = (TextView) getLayoutInflater().inflate(R.layout.item_tag, (ViewGroup) root);
+                tagView.setText(tagName);
+                tags.addView(tagView);
+            }
+        }
+
+        ImageSlider imageSlider = root.findViewById(R.id.slider);
+
+        List<SlideModel> slideModels = new ArrayList<>();
+        Bitmap bitmap = null;
+        slideModels.add(new SlideModel("https://p.ecopetit.cat/wpic/lpic/26-263518_tumblr-photography-wallpaper-rocks-on-earth-background.jpg",ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel("https://live.staticflickr.com/7006/6621416427_8504865e6a_z.jpg",ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel("https://c4.wallpaperflare.com/wallpaper/662/618/496/natur-2560x1600-sceneries-wallpaper-preview.jpg",ScaleTypes.CENTER_CROP));
+        imageSlider.setImageList(slideModels, ScaleTypes.CENTER_CROP);
     }
 }
