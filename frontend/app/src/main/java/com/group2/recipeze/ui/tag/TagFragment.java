093f613da7ffb788b3591ba32a3ac1b36d7b67b4
LouieMiddle <louiemiddle@yahoo.com>
2021-04-02 14:33:13 +0100

    Recycler View Populates

--- frontend/app/src/main/java/com/group2/recipeze/ui/tag/TagFragment.java
+++ frontend/app/src/main/java/com/group2/recipeze/ui/tag/TagFragment.java
@@ -1,9 +1,10 @@
-package com.group2.recipeze;
+package com.group2.recipeze.ui.tag;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,9 +15,11 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.group2.recipeze.R;
+import com.group2.recipeze.RecyclerViewAdapter;
 import com.group2.recipeze.data.RecipeRepository;
-import com.group2.recipeze.data.TagRepository;
 import com.group2.recipeze.data.model.Recipe;
+import com.group2.recipeze.endlessScroll;
 
 import java.util.ArrayList;
 
@@ -34,6 +37,20 @@ public class TagFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_tag, container, false);
         TagFragment thisFragment = this;
+
+        recipeRepository = RecipeRepository.getInstance();
+
+        recipes.observe(getViewLifecycleOwner(), new Observer<ArrayList<Recipe>>() {
+            @Override
+            public void onChanged(ArrayList<Recipe> recipes) {
+                // Populate endlessScroll with recipes
+                tagRecyclerView = root.findViewById(R.id.recipes);
+                endlessScrollManager = new endlessScroll(tagRecyclerView);
+                endlessScrollManager.initAdapter(thisFragment);
+                endlessScrollManager.initScrollListener();
+                endlessScrollManager.populateData(recipes);
+            }
+        });
 
         // Get tagName from whoever called fragment
         tagName = getArguments().getString("tagName");
@@ -61,6 +78,10 @@ public class TagFragment extends Fragment {
         });
 
         return root;
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        recipeRepository.getRecipesForTag(tagName, 0, recipes);
     }
 
 }
